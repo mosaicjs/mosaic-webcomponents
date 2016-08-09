625a738393070187e63d3703874dc4c95945f368
Mikhail Kotelnikov <mikhail.kotelnikov@gmail.com>
2016-08-09 12:03:50 +0200

    Refactor active fields

--- ActiveFields.js
+++ ActiveFields.js
@@ -7,99 +7,123 @@
         global[m.name] = m;
     }
 })(this, (function() {
-    return {
-        name : 'ActiveFields',
 
-        field : function(name, fields) {
-            return {
-                type : Object,
-                notify : true,
-                value : function() {
-                    var state = {};
-                    var obj = {};
-                    var toNotify = this._fieldsToNotify = this._fieldsToNotify
-                            || [];
-                    Object.keys(fields).forEach(function(field) {
-                        var f = {
-                            path : name + '.' + field,
-                            field : field,
-                            statePath : fields[field],
-                            changed : false,
-                            _value : undefined,
-                            cursor : undefined,
-                            setState : function(state) {
-                                f.cursor = state.cursor(f.statePath);
-                                if (f.changed) {
-                                    f.cursor(f._value);
-                                }
-                            },
-                            get : function() {
-                                return f.cursor ? f.cursor() : f._value;
-                            },
-                            set : function(_) {
-                                f.changed = true;
-                                if (f.cursor) {
-                                    f.cursor(_);
-                                } else {
-                                    f._value = _;
-                                }
-                                return f;
-                            }
-                        };
-                        toNotify.push(f);
-                        Object.defineProperty(obj, field, f);
-                    })
-                    return obj;
+    function ActiveFields(descriptions) {
+        var result = {};
+        var fields = Object.keys(descriptions);
+        fields.forEach(function(field) {
+            var descr = descriptions[field];
+            if (typeof descr === 'object' && descr.type === Object) {
+                var state = descr.state || descr.fields;
+                if (state) {
+                    descr.value = function() {
+                        this._fieldsToNotify = this._fieldsToNotify || [];
+                        var obj = defineObject(field, state,
+                                this._fieldsToNotify);
+                        return obj;
+                    }
                 }
             }
+            result[field] = descr;
+        });
+        return result;
+    }
+
+    function defineObject(name, fields, toNotify) {
+        var obj = {};
+        Object.keys(fields).forEach(function(field) {
+            var f = {
+                path : name + '.' + field,
+                field : field,
+                statePath : fields[field],
+                changed : false,
+                _value : undefined,
+                cursor : undefined,
+                setState : function(state) {
+                    f.cursor = state.cursor(f.statePath);
+                    if (f.changed) {
+                        f.cursor(f._value);
+                    }
+                },
+                get : function() {
+                    return f.cursor ? f.cursor() : f._value;
+                },
+                set : function(_) {
+                    f.changed = true;
+                    if (f.cursor) {
+                        f.cursor(_);
+                    } else {
+                        f._value = _;
+                    }
+                    return f;
+                }
+            };
+            Object.defineProperty(obj, field, f);
+            toNotify.push(f);
+        })
+        return obj;
+    }
+
+    ActiveFields.field = function(name, fields) {
+        return {
+            type : Object,
+            notify : true,
+            value : function() {
+                this._fieldsToNotify = this._fieldsToNotify || [];
+                var field = defineObject(name, fields, this._fieldsToNotify);
+                return field;
+            }
+        }
+    };
+
+    ActiveFields.Behavior = {
+
+        initActiveFields : function(state) {
+            this._stateSubscription = state.addListener(this._onStateChange,
+                    this);
+            if (this._fieldsToNotify) {
+                this._fieldsToNotify.forEach(function(field) {
+                    field.setState(state);
+                })
+            }
+            this._refreshFields();
         },
 
-        Behavior : {
+        clearActiveFields : function(state) {
+            if (this._stateSubscription) {
+                this._stateSubscription();
+                delete this._stateSubscription;
+            }
+            delete this._fieldsToNotify;
+        },
 
-            initActiveFields : function(state) {
-                this._stateSubscription = state.addListener(
-                        this._onStateChange, this);
-                if (this._fieldsToNotify) {
-                    this._fieldsToNotify.forEach(function(field) {
-                        field.setState(state);
-                    })
+        _onStateChange : function(ev) {
+            this._refreshFields();
+        },
+
+        _refreshFields : function() {
+            var fields = this._fieldsToNotify;
+            if (!fields || !fields.length)
+                return;
+            fields.forEach(function(field) {
+                var value = field._value;
+                var newValue = field._value = field.get();
+                if (newValue !== value) {
+                    this.notifyPath(field.path);
                 }
-                this._refreshFields();
-            },
+            }, this)
+        },
 
-            clearActiveFields : function(state) {
-                if (this._stateSubscription) {
-                    this._stateSubscription();
-                    delete this._stateSubscription;
-                }
-                delete this._fieldsToNotify;
-            },
+        onComponentAdd : function(state) {
+            this.initActiveFields(state);
+        },
 
-            _onStateChange : function(ev) {
-                this._refreshFields();
-            },
+        onComponentRemove : function(state) {
+            this.clearActiveFields(state);
+        },
 
-            _refreshFields : function() {
-                var fields = this._fieldsToNotify;
-                if (!fields || !fields.length)
-                    return;
-                fields.forEach(function(field) {
-                    var value = field._value;
-                    var newValue = field._value = field.get();
-                    if (newValue !== value) {
-                        this.notifyPath(field.path);
-                    }
-                }, this)
-            },
+    };
 
-            onComponentAdd : function(state) {
-                this.initActiveFields(state);
-            },
+    return ActiveFields;
 
-            onComponentRemove : function(state) {
-                this.clearActiveFields(state);
-            },
-
-        }
-    }
 })());
